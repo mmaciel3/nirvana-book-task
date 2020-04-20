0b0367adcfe232b82397f5e71b9a92facb3d9d5c
Matheus Soares <matheus.soares@adp.com>
2020-04-20 11:31:30 -0300

    Creating very simple UI

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
+app.use('/', express.static('public'));
 
 const validateRequiredFields = (req, res) => {
     const requiredFields = [
